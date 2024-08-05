29dbb9b659bfd6dad852b8e658c2398f973050ba
Apolisk <a.bogdanovitch8@gmail.com>
2024-08-05 19:19:14 +0300

    mods/monitor: implement

--- mods/monitor/monitor.go
+++ mods/monitor/monitor.go
@@ -2,6 +2,7 @@ package monitor
 
 import (
 	"automatica.team/plant"
+	"github.com/go-telebot/pkg/monitor"
 )
 
 func (mod *Monitor) Name() string {
@@ -18,5 +19,11 @@ func New() *Monitor {
 }
 
 func (mod *Monitor) Import(m plant.M) error {
+	mon, err := monitor.New(monitor.Config{URL: m.Get("url")})
+	if err != nil {
+		return err
+	}
+	mod.Use(mon.Middleware())
+
 	return nil
 }
